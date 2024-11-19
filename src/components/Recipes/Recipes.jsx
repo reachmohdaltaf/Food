c450b4487ba90fa89d454150e746e0711bce6590
reachmohdaltaf <reachmohdaltaf@gmail.com>
2024-11-19 15:11:15 +0530

    newww

--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -10,7 +10,7 @@ const Recipes = ({sampleData, addToCart}) => {
       <Header />
       <div className="recipes w-full bg-white h-full flex flex-col   items-center justify-center ">
         <div className="h-20 items-center flex justify-center bg-zinc-100 w-[90%] text-center">
-          <p className="text-3xl  font-bold">What On you mind?</p>
+          <p className="text-3xl  font-bold">What's On you mind?</p>
         </div>
         <div className="bg-zinc-100 w-[90%] p-10 items-center justify-center flex flex-wrap gap-10">
           {sampleData.map((recipe) => (
